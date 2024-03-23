1638c86a989e49fc2be46fd27eaffd7c8d8ad23e
Jamie Smith <thejamiemsmith@gmail.com>
2024-03-22 17:38:20 -0700

    feat(Dashboard): set up spotify api after grabbing access token

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,7 +1,22 @@
-export const Dashboard = () => {
+import config from '../../cfg/config.js';
+import SpotifyWebApi from 'spotify-web-api-node';
+import { useAuth } from '@/composables/useAuth.jsx';
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: config.clientId
+});
+export const Dashboard = ({ code }) => {
+	const accessToken = useAuth(code);
+	
+	useEffect(() => {
+		if (! accessToken) return;
+		spotifyApi.setAccessToken(accessToken);
+	}, [accessToken]);
+	
 	return (
 		<div>
 			<h1>Dashboard</h1>
+			{ accessToken }
 		</div>
 	)
 }
