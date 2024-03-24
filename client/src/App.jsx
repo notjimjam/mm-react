009924536b65ef2d6e2f8a0fadc82c4a96d5df23
Jamie Smith <thejamiemsmith@gmail.com>
2024-03-23 21:20:02 -0700

    feat(App): add header and footer to create general layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,20 @@
 import '@/assets/styles.less';
 import { Login } from '@/components/Login.jsx';
 import { Dashboard } from '@/components/Dashboard.jsx';
+import { Header } from '@/components/Header.jsx';
+import { Footer } from '@/components/Footer.jsx';
 
 const code = new URLSearchParams(window.location.search).get('code');
 
 function App() {
 
   return (
-    <div className='App'>
-	    {code ? <Dashboard code={code} /> : <Login />}
+    <div className='app'>
+	    <Header />
+	    <article id='article'>
+		    {code ? <Dashboard code={code} /> : <Login />}
+	    </article>
+	    <Footer />
     </div>
   )
 }
