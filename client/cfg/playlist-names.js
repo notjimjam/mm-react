1897f8ecbd42217efc031c839af54689319c3cdb
Jamie Smith <thejamiemsmith@gmail.com>
2024-03-26 15:51:57 -0700

    feat(PlaylistName): create file to hold weather playlist names

---
+++ client/cfg/playlist-names.js
@@ -0,0 +1,25 @@
+export default {
+	freezing: ['cold winter lofi', 'cold weather country', 'cold weather vibezz', 'for cold winter mornings', 'cold weather supremacy', 'the weather is getting colder', 'cold skies/grey weather'],
+	
+	sunny: ['sunny', 'good jams', 'a perfect day', 'indie sunshine', 'soak up the sun', 'uplifting soul classics', 'surf rock sunshine'],
+	
+	windy: ['windy days', 'windy weather', 'windy vibes', 'windy indie vibes'],
+	
+	partly: ['chill hits', 'just good music', 'partly cloudy', 'partly cloudy forecast', 'chilled r&b', "healin' blues"],
+	
+	hail: ['spring hail storm', 'southern gothic', 'violent classical'],
+	
+	heavy: ['tender', 'cozy rainy day', 'spring downpour', 'rainy day piano music', 'rainy day blues', 'sad soul'],
+	
+	rain: ['rainy day jazz, cold weather//rainy day', 'rainy day vibes', 'happy rainy day', 'chill rap on rainy days', 'sad girl country'],
+	
+	cloudy: ['calm before the storm', 'cloudy day', 'cloudy day lofi', 'slow cloudy mornings', 'overcast days', 'deep house relax', 'noir'],
+	
+	fog: ['foggy mornings', 'foggy forest vibes', 'morning fog', 'foggy mountain', 'misty forest', 'misty mountains', 'the misty moors'],
+	
+	snow: ['snowy mornings', 'snowy mornings in a coffee shop somewhere', 'snowy cabin in the mountains', 'chill snowy days', 'snowy jazz', 'snowy day vibes'],
+	
+	thunder: ['Dark and Stormy', 'stormy lofi', 'thunderstorm music', 'the thunderstorm playlist', 'mid:nite storms', 'villain mode', 'dark and moody deep house'],
+	
+	clear: ['the midnight hour', 'atmospheric piano', 'deep sleep', 'cosmic country', 'late night jazz', 'evening acoustic', 'moonlight beats'],
+}
