01f627bd514975d0c161189a0abf4780f4b5fd9f
Jamie Smith <thejamiemsmith@gmail.com>
2024-03-26 15:53:01 -0700

    feat(Login): add buttons to take user to login/signup

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,11 +2,37 @@ import config from '../../cfg/config.js';
 const authUrl = config.authUrl;
 const signUpUrl = config.signUpUrl;
 export const Login = () => {
+	
+	const loginHandler = (e) => {
+		e.preventDefault();
+		window.location.href = config.authUrl;
+	}
+	const signUpHandler = (e) => {
+		e.preventDefault();
+		window.location.href = config.signUpUrl;
+	}
+	
 	return (
-		<div>
-			<a href={authUrl}>
-				Log In With Spotify
-			</a>
+		<div className='flex justify-center items-center'>
+			<div className='card flex flex-col justify-center items-center gap-large'>
+				<div className='text-center'>
+					Playlists curated for your local weather
+					<br/>
+					powered by Spotify
+				</div>
+				<button
+					onClick={loginHandler}
+					className='button'
+				>
+					Log In With Spotify
+				</button>
+				<button
+					onClick={signUpHandler}
+					className='button'
+				>
+					Sign Up For Spotify
+				</button>
+			</div>
 		</div>
 	)
 }
