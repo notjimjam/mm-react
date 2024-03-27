efcc4f2ca2209a4a02d1bd80710f4d7c1f876848
Jamie Smith <thejamiemsmith@gmail.com>
2024-03-26 22:28:13 -0700

    feat(Login): swap logos, add app name to login card

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -14,24 +14,30 @@ export const Login = () => {
 	
 	return (
 		<div className='flex justify-center items-center'>
-			<div className='card flex flex-col justify-center items-center gap-large'>
-				<div className='text-center'>
-					Playlists curated for your local weather
-					<br/>
-					powered by Spotify
+			<div className='card flex flex-col justify-center items-center'>
+				<img
+					src='/logos/mm-logo-name.png'
+					alt='Music Meteorologist Logo'
+				/>
+				<div className='flex flex-col justify-center items-center gap-large'>
+					<div className='text-center'>
+						Playlists curated for your local weather
+						<br/>
+						powered by Spotify
+					</div>
+					<button
+						onClick={loginHandler}
+						className='button'
+					>
+						Log In With Spotify
+					</button>
+					<button
+						onClick={signUpHandler}
+						className='button'
+					>
+						Sign Up For Spotify
+					</button>
 				</div>
-				<button
-					onClick={loginHandler}
-					className='button'
-				>
-					Log In With Spotify
-				</button>
-				<button
-					onClick={signUpHandler}
-					className='button'
-				>
-					Sign Up For Spotify
-				</button>
 			</div>
 		</div>
 	)
