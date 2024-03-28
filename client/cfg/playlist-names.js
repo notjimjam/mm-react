c68d2fcc106ffe9a3f18856d6b8fd43afc62fdbb
Jamie Smith <thejamiemsmith@gmail.com>
2024-03-27 17:15:40 -0700

    chore(playlist-names): update names

--- client/cfg/playlist-names.js
+++ client/cfg/playlist-names.js
@@ -11,15 +11,15 @@ export default {
 	
 	heavy: ['tender', 'cozy rainy day', 'spring downpour', 'rainy day piano music', 'rainy day blues', 'sad soul'],
 	
-	rain: ['rainy day jazz, cold weather//rainy day', 'rainy day vibes', 'happy rainy day', 'chill rap on rainy days', 'sad girl country'],
+	rain: ["pov: it's raining🌙", 'cold weather// rainy day', 'songs for rainy car drives', 'Rainy Day Mix', 'chill rap on rainy days', 'rainy day old country🌾❤️'],
 	
-	cloudy: ['calm before the storm', 'cloudy day', 'cloudy day lofi', 'slow cloudy mornings', 'overcast days', 'deep house relax', 'noir'],
+	cloudy: ['calm before the storm', 'songs to play on a cloudy day', 'cloudy day lofi', 'gloomy morning', 'overcast days in winter', 'Cloudy Mornings☁️Playlists', 'noir/jazz lofi'],
 	
 	fog: ['foggy mornings', 'foggy forest vibes', 'morning fog', 'foggy mountain', 'misty forest', 'misty mountains', 'the misty moors'],
 	
 	snow: ['snowy mornings', 'snowy mornings in a coffee shop somewhere', 'snowy cabin in the mountains', 'chill snowy days', 'snowy jazz', 'snowy day vibes'],
 	
-	thunder: ['Dark and Stormy', 'stormy lofi', 'thunderstorm music', 'the thunderstorm playlist', 'mid:nite storms', 'villain mode', 'dark and moody deep house'],
+	thunder: ['Dark and Stormy', 'stormy lofi', 'thunderstorm mix', 'the thunderstorm playlist', 'mid:nite storms', 'villain mode', 'dark and moody deep house'],
 	
-	clear: ['the midnight hour', 'atmospheric piano', 'deep sleep', 'cosmic country', 'late night jazz', 'evening acoustic', 'moonlight beats'],
+	clear: ['late night slow chill', 'atmospheric piano', 'deep sleep', 'cosmic country', 'Night Time Vibes🌙', 'Mellow Evening', 'moonlight bumps'],
 }
