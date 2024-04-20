0196575150b3c05693ce19b9095458a30a2621b6
Jamie Smith <thejamiemsmith@gmail.com>
2024-04-20 00:05:55 -0700

    feat(MMButton): add docs

--- client/src/components/MMButton.jsx
+++ client/src/components/MMButton.jsx
@@ -1,3 +1,23 @@
+/**
+ * '@/components/MMButton.jsx'
+ * In components or pages:
+ *  <MMButton
+ * 	    clickFunction={ }
+ * 		extraClass=''
+ * 	>
+ * 		text
+ * 	</MMButton>
+ *
+ * Built a customized button to replace <button> tags
+ * Contains a clickFunction, extraClass, and children
+ *
+ * @param {children} - acts as a slot to display text or icons within the button
+ * @param {clickFunction} - the function to be executed on click
+ * @param {extraClass} - additional classes to be added to the button
+ * for styles and button sizing
+ *
+ * @returns {JSX.Element}
+ */
 export const MMButton = ({children, clickFunction, extraClass}) => {
 	const [wasJustClicked, setWasJustClicked] = useState(false);
 	
