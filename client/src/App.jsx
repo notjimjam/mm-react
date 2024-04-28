19e280e61cf0ec99ee9b806421c84b59b3bcc561
Jamie Smith <thejamiemsmith@gmail.com>
2024-04-28 15:54:09 -0700

    feat(App): add docs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,19 @@ import { Dashboard } from '@/components/Dashboard.jsx';
 import { Header } from '@/components/Header.jsx';
 import { Footer } from '@/components/Footer.jsx';
 
+/**
+ * retrieve code after sign in from the url to provide to Dashboard component
+ * url will contain 'code=...'
+ */
 const code = new URLSearchParams(window.location.search).get('code');
 
 function App() {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 	
+	/**
+	 * if code is present, provide to Dashboard component - else send to Login
+	 * component in order to sign in and retrieve a code
+	 */
 	useEffect(() => {
 		if (code) {
 			setIsLoggedIn(true);
