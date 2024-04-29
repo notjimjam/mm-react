18fc3c6bd005be27e375ab2bd67834b3780c2876
Jamie Smith <thejamiemsmith@gmail.com>
2024-04-29 16:13:41 -0700

    feat(Player): add docs

--- client/src/components/Player.jsx
+++ client/src/components/Player.jsx
@@ -3,13 +3,24 @@ import SpotifyWebPlayback from 'react-spotify-web-playback';
 export const Player = ({ accessToken, trackUri }) => {
 	const [play, setPlay] = useState(false);
 	
-	//if a trackUri is passed, setPlay to true
-	//this allows for autoplay of the playlist
+	/**
+	 * when trackUri is updated, set play to true
+	 * this will allow the track to automatically play
+	 */
 	useEffect(() => setPlay(true), [trackUri]);
 	
 	if (! accessToken) return null;
 	
 	return (
+		/**
+		 * SpotifyWebPlayback component
+		 * token: accessToken
+		 * callback: when the track is paused, set play to false
+		 * play: play state
+		 * uris: trackUri
+		 * component is brought in from react-spotify-web-playback  
+		 * https://www.npmjs.com/package/react-spotify-web-playback
+		 */
 		<SpotifyWebPlayback
 			token={accessToken}
 			callback={(state) => {
